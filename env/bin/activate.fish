87da3c581178b1b59944a125be4655203dbfba00
Eshan <eshan.tewari@gmail.com>
2017-03-11 10:42:49 -0500

    Near Final Draft

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/eshan/Desktop/NCAA/env"
+set -gx VIRTUAL_ENV "/Users/eshan/Desktop/NCAA_Simulation/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- env/bin/django-admin.py
+++ env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/eshan/Desktop/NCAA/env/bin/python2.7
+#!/Users/eshan/Desktop/NCAA_Simulation/env/bin/python2.7
 from django.core import management
 
 if __name__ == "__main__":
